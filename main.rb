02b9cfbb1785f07c30d5a68480349cc020e7db5f
Jamal Powell <jamalkpowell@gmail.com>
2013-06-25 11:25:47 -0400

    added a calculator page

--- main.rb
+++ main.rb
@@ -2,5 +2,42 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  "Hello!"
+  "Hello! what it do"
 end
+
+get '/hello_world' do
+  "Hello, World".reverse.upcase
+end
+
+get '/hello_me' do
+  @me = "Jamal"
+  return "Hello, world #{@me.upcase}"
+end
+
+get '/hello_you/:name' do
+  @name = params[:name]
+  return "Hello, #{@name}"
+end
+
+
+get '/names/:first/:last' do
+  @first = params[:first]
+  @second = params[:second]
+  return "you can do multiple parameters,
+         #{@first}, #{@last}!"
+end
+
+get '/calc/add/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  # Sinatra wants to put a string.
+  return (@first + @second).to_s
+end
+
+get '/calc/multiply/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  # Sinatra wants to put a string.
+  return (@first * @second).to_s
+end
+
